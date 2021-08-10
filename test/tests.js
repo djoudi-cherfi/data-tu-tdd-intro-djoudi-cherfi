1eb1d5c7bc8732aba953f62b3ada49c5d0e5d6d1
DjoudiDev <djoudi.cherfi@gmail.com>
2021-08-10 14:50:32 +0200

    feat(chai) add chai and update getRandomFirstName BREAKING CHANGE: mise en place de chai et mise à jour de la fonction getRandomFirstName :triangular_flag_on_post:

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,5 @@
+const { expect } = require('chai');
+
 const assert = require('assert');
 
 const { getRandomFirstName, data } = require('../services/randomFirstName');
@@ -5,15 +7,15 @@ const { getRandomFirstName, data } = require('../services/randomFirstName');
 describe('randomFirstName service', function() {
     describe('getRandomFirstName', function() {
         it('should return a string', function() {
-            assert.strictEqual(typeof getRandomFirstName(), 'string');
+            expect(getRandomFirstName()).to.be.a('string');
         });
 
         it('should return a first name made up of letters, spaces and dashes only', function() {
-            assert.match(getRandomFirstName(), /^[a-zA-Z -]+$/);
+            expect(getRandomFirstName()).to.match(/^[a-zA-Z -]+$/);
         });
 
         it('should return one of the proposition in data', function() {
-            assert.ok(data.includes(getRandomFirstName()));
+            expect(getRandomFirstName()).to.be.oneOf(data);
         });
     });
 });
