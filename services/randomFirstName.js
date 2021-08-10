12a485a87911510eb3ff538a4a92b1f3120ce5d6
DjoudiDev <djoudi.cherfi@gmail.com>
2021-08-10 14:01:39 +0200

    feat(mocha) update getRandomFirstName BREAKING CHANGE: mise à jour de la fonction getRandomFirstName :triangular_flag_on_post:

--- services/randomFirstName.js
+++ services/randomFirstName.js
@@ -1,5 +1,8 @@
 const data = ['Barack', 'Serena', 'Jean', 'Paul', 'Jean-Paul'];
 
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 module.exports = {
-    getRandomFirstName: () => 'Georges'
+    getRandomFirstName: () => data[random(0, data.length - 1)],
+    data
 };
--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const { getRandomFirstName } = require('../services/randomFirstName');
+const { getRandomFirstName, data } = require('../services/randomFirstName');
 
 describe('randomFirstName service', function() {
     describe('getRandomFirstName', function() {
@@ -8,24 +8,12 @@ describe('randomFirstName service', function() {
             assert.strictEqual(typeof getRandomFirstName(), 'string');
         });
 
-        it('should return a firstname', function() {
+        it('should return a first name made up of letters, spaces and dashes only', function() {
             assert.match(getRandomFirstName(), /^[a-zA-Z -]+$/);
+        });
+
+        it('should return one of the proposition in data', function() {
+            assert.ok(data.includes(getRandomFirstName()));
         });
     });
 });
-
-// Test 1 : la fonction retourne une string
-// try {
-//     assert.strictEqual(typeof getRandomFirstName(), 'string', 'La fonction pickRandomFirstname devrait retourner une string');
-//     console.log('Test 1 ok');
-// } catch (error) {
-//     console.error(error.message);
-// }
-
-// try {
-//     // Test 2 : la string est un prénom
-//     assert.match(getRandomFirstName(), /^[a-zA-Z -]+$/, "La fonction pickRandomFirstname devrait retourner un prénom composé de lettres, d'espaces et de tirets uniquement");
-//     console.log('Test 2 ok');
-// } catch (error) {
-//     console.error(error.message);
-// }
